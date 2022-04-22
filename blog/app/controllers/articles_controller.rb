4fdc31c849f30e84812da574b85903af024b118b
Jordão Rodrigues Oliveira Rosario <jordao.rosario01@gmail.com>
2022-04-22 09:26:54 -0300

    I think we finished the getting started guide

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
   def index
     @articles = Article.all
   end
@@ -41,8 +42,8 @@ class ArticlesController < ApplicationController
     redirect_to root_path, status: :see_other
   end
 
-  private 
+  private
     def article_params
-      params.require(:article).permit(:title, :body)
+      params.require(:article).permit(:title, :body, :status)
     end
 end
